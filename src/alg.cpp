6995b79a22504aaf1a48f7161cb74d441bf646c8
fakemove <160417424+fakemove@users.noreply.github.com>
2024-03-05 15:05:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,19 +4,17 @@
 
 
 double pown(double value, uint16_t n) {
-  double res = value;
-  while (n != 1) {
+double res = 1.0;
+  for (uint16_t i = 1; i <= n; i++)
     res *= value;
-    n -= 1;
-  }
   return res;
 }
 
 uint64_t fact(uint16_t n) {
-  if (n <= 1) {
-	  return 1;
-	}
-	return n * fact(n - 1);
+  uint64_t res = 1;
+  for (uint16_t i = 1; i <= n; i++)
+    res *= i;
+  return res;
 }
 
 double calcItem(double x, uint16_t n) {
@@ -24,25 +22,22 @@ double calcItem(double x, uint16_t n) {
 }
 
 double expn(double x, uint16_t count) {
-  double res = 1;
-  for (uint16_t i = 1; i <= count; i++) {
-	  res += calcItem(x, i);
-  }
+  double res = 0.0;
+  for (uint16_t i = 0; i <= count; i++)
+    res += calcItem(x, i);
   return res;
 }
 
 double sinn(double x, uint16_t count) {
   double res = 0.0;
-  for (uint16_t i = 1; i <= count; i++) {
+  for (uint16_t i = 1; i <= count; i++)
     res += pown(-1, i - 1) * calcItem(x, 2 * i - 1);
-  }
   return res;
 }
 
 double cosn(double x, uint16_t count) {
   double res = 0.0;
-  for (uint16_t i = 1; i <= count; i++) {
-	  res += pown(-1, i - 1) * calcItem(x, 2 * i - 2);
-  }
+  for (uint16_t i = 1; i <= count; i++)
+    res += pown(-1, i - 1) * calcItem(x, 2 * i - 2);
   return res;
 }
